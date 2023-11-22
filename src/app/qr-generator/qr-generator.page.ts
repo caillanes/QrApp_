6a72e11844d1acfa29d79f9cfc0ac704a4678c8f
caillanes <ca.illanes@duocuc.cl>
2023-11-22 15:35:57 -0300

    wazaaaa

--- src/app/qr-generator/qr-generator.page.ts
+++ src/app/qr-generator/qr-generator.page.ts
@@ -18,26 +18,27 @@ export class QrGeneratorPage implements OnInit {
   constructor(private animationCtrl: AnimationController, private menu: MenuController) { }
 
   ngAfterViewInit() {
-    const card = this.animationCtrl
-      .create()
-      .addElement(
-        this.cardElements.get(0)!.nativeElement)
-      .duration(2000)
-      .beforeStyles({
-        filter: 'invert(75%)',
-      })
-      .beforeClearStyles(['box-shadow'])
-      .afterStyles({
-        'box-shadow': 'rgba(255, 0, 50, 0.4) 0px 4px 16px 6px',
-      })
-      .afterClearStyles(['filter'])
-      .keyframes([
-        { offset: 0, transform: 'scale(1)' },
-        { offset: 0.5, transform: 'scale(1.5)' },
-        { offset: 1, transform: 'scale(1)' },
-      ]);
-
-    this.animation = this.animationCtrl.create().duration(2000).addAnimation([card]);
+    if (this.cardElements && this.cardElements.length > 0) {
+      const card = this.animationCtrl
+        .create()
+        .addElement(this.cardElements.first.nativeElement)
+        .duration(2000)
+        .beforeStyles({
+          filter: 'invert(75%)',
+        })
+        .beforeClearStyles(['box-shadow'])
+        .afterStyles({
+          'box-shadow': 'rgba(255, 0, 50, 0.4) 0px 4px 16px 6px',
+        })
+        .afterClearStyles(['filter'])
+        .keyframes([
+          { offset: 0, transform: 'scale(1)' },
+          { offset: 0.5, transform: 'scale(1.5)' },
+          { offset: 1, transform: 'scale(1)' },
+        ]);
+  
+      this.animation = this.animationCtrl.create().duration(2000).addAnimation([card]);
+    }
   }
 
   play() {
